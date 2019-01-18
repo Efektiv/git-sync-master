bee400f15f4f5c225fefca72af3013372125e473
crowncodes <40325827+crowncodes@users.noreply.github.com>
2019-01-18 15:35:33 -0500

    Update git-sync.py

--- git-sync.py
+++ git-sync.py
@@ -79,10 +79,7 @@ def setup_repo(repo, dest, branch):
         
         output = sh(['git', 'pull'], cwd=dest)
         output = sh(['git', 'add', '.'], cwd=dest)
-        output = sh(['git', 'add', '-u'], cwd=dest)     
-        output = sh(['git', 'config', '--global', '--edit'], cwd=dest)
-        output = sh(['git', 'commit', '-ca'], cwd=dest)
-        output = sh(['git', 'push'], cwd=dest)
+        output = sh(['git', 'add', '-u'], cwd=dest)
 
 def sync_repo(repo, dest, branch, rev):
     """
