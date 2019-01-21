187e3e06c66ec7a37a2e2c2813867bf85434ba93
crowncodes <40325827+crowncodes@users.noreply.github.com>
2019-01-21 10:15:36 -0500

    Update git-sync.py

--- git-sync.py
+++ git-sync.py
@@ -42,7 +42,7 @@ def setup_repo(repo, dest, branch):
     repo_name = urlparse(repo).path
     
     # if no git repo exists at dest, clone the requested repo
-    if not os.path.exists(os.path.join(dest, '.git')):
+    if not os.path.exists(os.path.join('/git/', '.git')):
         output = sh(
             ['git', 'clone', '--no-checkout', '-b', branch, repo, dest])
         click.echo('Cloned ...{repo_name}'.format(**locals()))
